21b05d8da738eb447fbd0c2af4e022cb7ddfc619
Kris Davison <kris.davison@digital.hmrc.gov.uk>
2017-06-22 14:00:36 +0100

    AFID-10 service layer functions defined -work in progress-

--- app/uk/gov/hmrc/taxhistory/services/EmploymentHistoryService.scala
+++ app/uk/gov/hmrc/taxhistory/services/EmploymentHistoryService.scala
@@ -16,20 +16,20 @@
 
 package uk.gov.hmrc.taxhistory.services
 
+
 import play.api.http.Status
-
-
 import uk.gov.hmrc.play.http.{HeaderCarrier, HttpResponse}
 import uk.gov.hmrc.tai.model.rti.RtiData
 import uk.gov.hmrc.taxhistory.connectors.des.RtiConnector
 import uk.gov.hmrc.taxhistory.connectors.nps.EmploymentsConnector
 import uk.gov.hmrc.taxhistory.model.nps.NpsEmployment
-import uk.gov.hmrc.taxhistory.model.taxhistory.Employment
 import uk.gov.hmrc.time.TaxYear
 import play.api.http.Status._
+import play.api.libs.json.Json
 import uk.gov.hmrc.domain.Nino
-import scala.concurrent.ExecutionContext.Implicits.global
+import uk.gov.hmrc.taxhistory.model.taxhistory.Employment
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 object EmploymentHistoryService extends EmploymentHistoryService
@@ -37,8 +37,68 @@ object EmploymentHistoryService extends EmploymentHistoryService
 trait EmploymentHistoryService {
   def employmentsConnector : EmploymentsConnector = EmploymentsConnector
   def rtiConnector : RtiConnector = RtiConnector
+  lazy val notFoundMessage = Json.parse("")
 
-  def getEmploymentHistory(nino:String, taxYear:Int): Future[HttpResponse] = ???
+  def getEmploymentHistory(nino:String, taxYear:Int)(implicit headerCarrier: HeaderCarrier): Future[HttpResponse] = {
+    val validatedNino = Nino(nino)
+    val validatedTaxYear = TaxYear(taxYear)
+
+    val x = for {
+                  npsEmploymentsFuture <- getNpsEmployments(validatedNino, validatedTaxYear)
+                }
+                yield {
+                  npsEmploymentsFuture match {
+                    case Left(httpResponse) =>Future.successful(httpResponse)
+                    case Right(Nil) => Future.successful(HttpResponse(Status.NOT_FOUND, Some(notFoundMessage)))
+                    case Right(npsEmploymentList) => {
+                      handleNpsEmploymentList(validatedNino, validatedTaxYear)(npsEmploymentList, createEmploymentList)
+                    }
+                  }
+                }
+    x.flatMap(identity)
+  }
+
+  def handleNpsEmploymentList(nino:Nino,taxYear:TaxYear)
+                             (npsEmploymentList:List[NpsEmployment],
+                              mergeEmployments: (RtiData,List[NpsEmployment]) => List[Employment])
+                             (implicit headerCarrier: HeaderCarrier): Future[HttpResponse] = {
+    for(rtiDataFuture <- getRtiEmployments(nino, taxYear))
+      yield {
+        rtiDataFuture match {
+          case Left(httpResponse) =>httpResponse
+          case Right(rtiData) => {
+            HttpResponse(Status.OK,
+              Some(Json.toJson(mergeEmployments(rtiData,npsEmploymentList))))
+          }
+        }
+      }
+  }
+
+  def createEmploymentList(rtiData:RtiData, npsEmployments: List[NpsEmployment]): List[Employment] = {
+    npsEmployments.flatMap {
+      npsEmployment => {
+        rtiData.employments.filter(
+          rtiEmployment => {
+            rtiEmployment.sequenceNo == npsEmployment.sequenceNumber &&
+            rtiEmployment.payeRef == npsEmployment.payeNumber &&
+            rtiEmployment.officeNumber == npsEmployment.taxDistrictNumber
+          }
+        ) match {
+            case Nil => None
+            case start :: end => None //TODO mmmm???
+            case matchingEmp :: Nil => {
+              val payment = matchingEmp.payments.sorted.last //TODO deal with no payments
+              Some(Employment(
+                employerName = npsEmployment.employerName,
+                payeReference = npsEmployment.payeNumber,
+                taxablePayTotal = payment.taxablePayYTD,
+                taxTotal = payment.totalTaxYTD
+              ))
+            }
+        }
+      }
+    }
+  }
 
   def getNpsEmployments(nino:Nino, taxYear:TaxYear)(implicit hc: HeaderCarrier): Future[Either[HttpResponse ,List[NpsEmployment]]] = {
     employmentsConnector.getEmployments(nino,taxYear.currentYear).map{
