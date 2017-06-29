62286e88517802e88dc8eeaa19bc5baa014609bd
shaileshcpatil <shailesh.patil@digital.hmrc.gov.uk>
2017-06-29 09:44:51 +0100

    Added the Auth

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -17,7 +17,8 @@ object MicroServiceBuild extends Build with MicroService {
     "uk.gov.hmrc" %% "play-config" % "4.3.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
     "uk.gov.hmrc" %% "domain" % "4.1.0",
-    "uk.gov.hmrc" %% "tax-year" % "0.3.0"
+    "uk.gov.hmrc" %% "tax-year" % "0.3.0",
+    "uk.gov.hmrc" %% "play-auth" % "1.2.0"
   )
 
   def test(scope: String = "test,it") = Seq(
