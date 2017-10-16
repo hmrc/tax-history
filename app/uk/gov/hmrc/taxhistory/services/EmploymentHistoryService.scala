b772a93076f724bc9929931a603c5c6d8b6b5f1d
Kris Davison <kris.davison@digital.hmrc.gov.uk>
2017-10-16 16:59:35 +0100

    AFID-150 refined mock return values

--- app/uk/gov/hmrc/taxhistory/services/EmploymentHistoryService.scala
+++ app/uk/gov/hmrc/taxhistory/services/EmploymentHistoryService.scala
@@ -60,7 +60,7 @@ trait EmploymentHistoryService extends EmploymentHistoryServiceHelper {
   }
 
   def getAllowances(nino:String, taxYear:Int)(implicit headerCarrier: HeaderCarrier): Future[HttpResponse] = {
-    val allowances = List(new Allowance(iabdType = "someMockType", amount=BigDecimal(11)))
+    val allowances = List(new Allowance(iabdType = "FlatRateJobExpenses", amount=BigDecimal(11)))
     //TODO remove mock stub allowances
     Future.successful(HttpResponse(Status.OK,Some(Json.toJson(allowances))))
   }
