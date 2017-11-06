cd92f63bec97105fdf4ea989fc8098418f1bc937
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2017-11-06 16:58:35 +0000

    AFID-130:add response body to NotFound

--- app/uk/gov/hmrc/taxhistory/controllers/PayAndTaxController.scala
+++ app/uk/gov/hmrc/taxhistory/controllers/PayAndTaxController.scala
@@ -42,8 +42,8 @@ trait PayAndTaxController extends AuthController {
           case OK => Ok(response.body)
           case NOT_FOUND => NotFound(response.body)
           case BAD_REQUEST => BadRequest(response.body)
-          case SERVICE_UNAVAILABLE => ServiceUnavailable(response.body)
-          case _ => InternalServerError(response.body)
+          case SERVICE_UNAVAILABLE => ServiceUnavailable
+          case _ => InternalServerError
         }
     }
   }
