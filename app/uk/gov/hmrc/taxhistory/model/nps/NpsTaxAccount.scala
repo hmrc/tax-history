9f9f33c0e083f8ea3fbb81249fc6858e0ee5c295
Shailesh Patil <shailesh.patil@digital.hmrc.gov.uk>
2018-01-03 12:44:13 +0000

    Updated the test for GetTaxAccount

--- app/uk/gov/hmrc/taxhistory/model/nps/NpsTaxAccount.scala
+++ app/uk/gov/hmrc/taxhistory/model/nps/NpsTaxAccount.scala
@@ -1,35 +1,63 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.taxhistory.model.nps
 
-import play.api.libs.json._
-import uk.gov.hmrc.tai.model.rti.{RtiEarlierYearUpdate, RtiEmployment, RtiPayment}
 
-case class NpsTaxAccount(employmentSequenceNumber:Option[Int],
-                         outstandingDebtRestriction: Option[BigDecimal],
-                         underpaymentAmount: Option[BigDecimal],
-                         actualPUPCodedInCYPlusOneTaxYear: Option[BigDecimal])
+import play.api.libs.json._
+
+
+
+case class Deduction(`type`:Int,sourceAmount:Option[BigDecimal])
+
+object Deduction {
+  implicit val formats = Json.format[Deduction]
+}
+case class IncomeSource(employmentId:Int,
+                        employmentType:Int,actualPUPCodedInCYPlusOneTaxYear:Option[BigDecimal],
+                        deductions: List[Deduction])
+
+object IncomeSource {
+  implicit val formats = Json.format[IncomeSource]
+}
+
+case class NpsTaxAccount(incomeSources: List[IncomeSource]){
+
+   val PrimaryEmployment = 1
+   val OutStandingDebtType = 41
+   val UnderpaymentAmountType = 35
+
+
+  def getOutStandingDebt()={
+    incomeSources.find(_.employmentType==PrimaryEmployment).
+      flatMap(_.deductions.find(_.`type` == OutStandingDebtType)).flatMap(_.sourceAmount)
+  }
+
+  def getUnderPayment()={
+    incomeSources.find(_.employmentType==PrimaryEmployment).
+      flatMap(_.deductions.find(_.`type` == UnderpaymentAmountType)).flatMap(_.sourceAmount)
+  }
+  def getActualPupCodedInCYPlusOne()={
+    incomeSources.find(_.employmentType==PrimaryEmployment).flatMap(_.actualPUPCodedInCYPlusOneTaxYear)
+  }
+
+
+}
 
 object NpsTaxAccount {
-  implicit val reader = new Reads[NpsTaxAccount] {
-    def reads(js: JsValue): JsResult[NpsTaxAccount] =
-    {
-
-
-        for {
-          employmentSequenceNumber <- (js \ "sequenceNumber" ).validateOpt[Int]
-          outstandingDebtRestriction <- (js \ "empRefs" \ "officeNo").validateOpt[BigDecimal]
-          underpaymentAmount <- (js \ "empRefs" \ "payeRef").validateOpt[BigDecimal]
-          actualPUPCodedInCYPlusOneTaxYear <- (js \ "currentPayId").validateOpt[BigDecimal]
-        } yield {
-          NpsTaxAccount(
-            employmentSequenceNumber = Some(employmentSequenceNumber),
-            outstandingDebtRestriction = Some(outstandingDebtRestriction),
-            underpaymentAmount = Some(underpaymentAmount),
-            actualPUPCodedInCYPlusOneTaxYear = Some(actualPUPCodedInCYPlusOneTaxYear)
-          )
-        }
-
-    }
-  }
-  implicit val writer = Json.writes[NpsTaxAccount]
+  implicit val formats = Json.format[NpsTaxAccount]
 }
 
