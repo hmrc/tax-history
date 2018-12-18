e0efa20a77f43cfc88168d4b18ea2f75c2e5e9a0
John Revill <john.revill@digital.hmrc.gov.uk>
2018-12-18 17:29:18 +0000

    Removed accidental file from commit

--- test/uk/gov/hmrc/taxhistory/services/EmploymentMatchingHelperSpec.scala
+++ test/uk/gov/hmrc/taxhistory/services/EmploymentMatchingHelperSpec.scala
@@ -16,13 +16,11 @@
 
 package uk.gov.hmrc.taxhistory.services
 
-import org.joda.time.LocalDate
 import org.scalatest.BeforeAndAfterEach
 import org.scalatestplus.play.PlaySpec
 import uk.gov.hmrc.http.HeaderCarrier
-import uk.gov.hmrc.tai.model.rti.{RtiData, RtiEmployment, RtiPayment}
+import uk.gov.hmrc.tai.model.rti.RtiData
 import uk.gov.hmrc.taxhistory.fixtures.{NpsEmployments, RtiEmployments}
-import uk.gov.hmrc.taxhistory.model.nps.EmploymentStatus.Ceased
 import uk.gov.hmrc.taxhistory.model.nps.NpsEmployment
 import uk.gov.hmrc.taxhistory.model.utils.TestUtil
 import uk.gov.hmrc.taxhistory.services.helpers.EmploymentMatchingHelper
@@ -33,80 +31,6 @@ class EmploymentMatchingHelperSpec extends PlaySpec with TestUtil with BeforeAnd
   private val testNino = randomNino()
 
   "EmploymentMatchingHelper" should {
-    "Given 2 NPS employments for the same employer and only 1 RTI employment, only match 1 of those 2 NPS employments to the RTI employment" in {
-      val rti1 = RtiEmployment(
-        payeRef = "U313",
-        officeNumber = "951",
-        currentPayId = Some("52236"),
-        sequenceNo = 3,
-        payments = List(
-          RtiPayment(
-            paidOnDate = LocalDate.parse("2014-12-28"),
-            taxablePayYTD = BigDecimal("760.00"),
-            totalTaxYTD = BigDecimal("152.00"),
-            studentLoansYTD = Some(BigDecimal("0.00"))
-          ),
-          RtiPayment(
-            paidOnDate = LocalDate.parse("2015-01-28"),
-            taxablePayYTD = BigDecimal("760.00"),
-            totalTaxYTD = BigDecimal("152.00"),
-            studentLoansYTD = Some(BigDecimal("0.00"))
-          ),
-          RtiPayment(
-            paidOnDate = LocalDate.parse("2015-03-28"),
-            taxablePayYTD = BigDecimal("1520.00"),
-            totalTaxYTD = BigDecimal("304.00"),
-            studentLoansYTD = None
-          ),
-          RtiPayment(
-            paidOnDate = LocalDate.parse("2015-03-28"),
-            taxablePayYTD = BigDecimal("1520.00"),
-            totalTaxYTD = BigDecimal("304.00"),
-            studentLoansYTD = None
-          )
-        ),
-        earlierYearUpdates = Nil
-      )
-
-      val nps1 = NpsEmployment(
-        payeNumber = "U313",
-        taxDistrictNumber = "951",
-        worksNumber = Some("52236"),
-        sequenceNumber = 3,
-        nino = "AA000000A",
-        employerName = "Employer 1",
-        receivingJobSeekersAllowance = false,
-        otherIncomeSourceIndicator = false,
-        startDate = Some(LocalDate.parse("2014-09-01")),
-        endDate = Some(LocalDate.parse("2018-07-28")),
-        receivingOccupationalPension = false,
-        employmentStatus = Ceased
-      )
-
-      val nps2 = NpsEmployment(
-        payeNumber = "U313",
-        taxDistrictNumber = "951",
-        worksNumber = Some("51305"),
-        sequenceNumber = 1,
-        nino = "AA000000A",
-        employerName = "Employer 1",
-        receivingJobSeekersAllowance = false,
-        otherIncomeSourceIndicator = false,
-        startDate = Some(LocalDate.parse("2011-09-11")),
-        endDate = Some(LocalDate.parse("2015-04-05")),
-        receivingOccupationalPension = false,
-        employmentStatus = Ceased
-      )
-
-      val rti = List(rti1)
-      val nps = List(nps1, nps2)
-
-      EmploymentMatchingHelper.ambiguousEmploymentMatches(nps, rti) must be(1)
-
-      EmploymentMatchingHelper.matchedEmployments(nps, rti).size must be(1)
-
-    }
-
     "successfully merge if there are multiple matching rti employments for a single nps employment1" when {
       val rtiData = rtiPartialDuplicateEmploymentsResponse.as[RtiData]
       val npsEmployments = npsEmploymentResponse.as[List[NpsEmployment]]
