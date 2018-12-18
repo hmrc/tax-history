9a67550b51f9857c44d262021d689e3772d6a6da
John Revill <johnrevill@users.noreply.github.com>
2018-12-18 17:31:16 +0000

    [ASA-445][JR,RV] Added test-only endpoint to clear cache (#118)

---
+++ app/uk/gov/hmrc/taxhistory/controllers/TestOnlyController.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.taxhistory.controllers
+
+import javax.inject.Inject
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.taxhistory.services.TaxHistoryMongoCacheService
+
+
+class TestOnlyController @Inject()(val cacheService: TaxHistoryMongoCacheService) extends TaxHistoryController {
+
+  def clearCache: Action[AnyContent] = Action.async { implicit request =>
+    import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext.fromLoggingDetails
+    cacheService.cacheRepository.drop.map(_ => Ok)
+  }
+}
