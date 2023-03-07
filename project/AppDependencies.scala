a071196d05bf135a92044ff74a00a45d57e50922
Gaston Racui <gastonracui@gmail.com>
2023-03-07 14:58:42 +0000

    DDCE-3563 Upgrade to scala 2.13

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,6 @@ import sbt._
 
 object AppDependencies {
 
-  private lazy val silencerVersion      = "1.7.9"
   private lazy val hmrcMongoPlayVersion = "0.71.0"
 
   //TODO migrate from jodaTime to JavaTime
@@ -12,11 +11,9 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "7.2.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoPlayVersion,
     "uk.gov.hmrc"       %% "domain"                    % "8.1.0-play-28",
-    "uk.gov.hmrc"       %% "agent-mtd-identifiers"     % "0.47.0-play-28",
-    "uk.gov.hmrc"       %% "tax-year"                  % "1.1.0", // 1.2.0 moved to java.time instead of org.joda.time which we don't want
-    "com.typesafe.play" %% "play-json-joda"            % "2.9.3",
-    compilerPlugin("com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full),
-    "com.github.ghik"    % "silencer-lib"              % silencerVersion % Provided cross CrossVersion.full
+    "uk.gov.hmrc"       %% "agent-mtd-identifiers"     % "0.58.0-play-28", //note that this doesnt exist with scala 2.13
+    "uk.gov.hmrc"       %% "tax-year"                  % "3.0.0", // 1.2.0 moved to java.time instead of org.joda.time which we don't want
+    "com.typesafe.play" %% "play-json-joda"            % "2.9.4",
   )
 
   private val test: Seq[ModuleID] = Seq(
@@ -25,7 +22,7 @@ object AppDependencies {
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0",
     "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0",
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2",
-    "org.mockito"             % "mockito-core"            % "4.7.0"
+    "org.mockito"             % "mockito-core"            % "5.1.1"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID]      = compile ++ test
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,15 +3,20 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts
   Resolver.ivyStylePatterns
 )
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.8.0")
+// To resolve a bug with version 2.x.x of the scoverage plugin - https://github.com/sbt/sbt/issues/6997
+// Try to remove when sbt 1.8.0+ and scoverage is 2.0.7+
+ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
 
-addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "2.0.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.9.0")
 
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
+addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "2.0.7")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.1.0")
+addSbtPlugin("com.beautiful-scala" % "sbt-scalastyle" % "1.5.1")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.16")
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.2.0")
+
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.19")
 
 addSbtPlugin("com.timushev.sbt" % "sbt-updates"  % "0.6.3")
-addSbtPlugin("org.scalameta"    % "sbt-scalafmt" % "2.4.6")
+
+addSbtPlugin("org.scalameta"    % "sbt-scalafmt" % "2.5.0")
