757c71a8945f9d210fc3497a1241d498914cf7dd
Victor Osipov <osipovvictor1994@gmail.com>
2023-10-27 09:56:59 +0100

    DDCE-4795 - Show tax breakdown for previous years

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,9 +1,9 @@
 import play.sbt.PlayImport.ws
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private lazy val hmrcBootstrapPlayVersion = "7.20.0"
+  private lazy val hmrcBootstrapPlayVersion = "7.22.0"
   private lazy val hmrcMongoPlayVersion     = "1.3.0"
 
   private val compile = Seq(
@@ -12,17 +12,15 @@ object AppDependencies {
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoPlayVersion,
     "uk.gov.hmrc"       %% "domain"                    % "8.3.0-play-28",
     "uk.gov.hmrc"       %% "tax-year"                  % "3.3.0",
-    "uk.gov.hmrc"       %% "agent-mtd-identifiers"     % "1.13.0"
+    "uk.gov.hmrc"       %% "agent-mtd-identifiers"     % "1.15.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % hmrcBootstrapPlayVersion,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % hmrcMongoPlayVersion,
-    "org.scalatest"          %% "scalatest"               % "3.2.16",
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0",
-    "org.scalatestplus"      %% "mockito-4-6"             % "3.2.15.0",
-    "com.vladsch.flexmark"    % "flexmark-all"            % "0.64.8",
-    "org.scalamock"          %% "scalamock"               % "5.2.0"
+    "uk.gov.hmrc"         %% "bootstrap-test-play-28"  % hmrcBootstrapPlayVersion,
+    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-28" % hmrcMongoPlayVersion,
+    "org.scalatest"       %% "scalatest"               % "3.2.17",
+    "org.scalatestplus"   %% "mockito-4-11"            % "3.2.17.0",
+    "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID]      = compile ++ test
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sm2 --stop AGENT_ACCESS_CONTROL
-sm2 --start AGENT_ACCESS_CONTROL -r 0.81.0
-sbt run
