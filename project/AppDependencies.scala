1170075a5cae0f033784b71cc20983e84319cbf2
Divya Manivannan <175815017+Divya-Manivannan-hmrc@users.noreply.github.com>
2025-07-24 12:08:46 +0100

    DDCE-6926 - Fix Missing Student Loan Data (#189)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val hmrcBootstrapPlayVersion = "9.13.0"
-  private lazy val hmrcMongoPlayVersion     = "2.6.0"
+  private lazy val hmrcBootstrapPlayVersion = "9.18.0"
+  private lazy val hmrcMongoPlayVersion     = "2.7.0"
 
   private val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % hmrcBootstrapPlayVersion,
