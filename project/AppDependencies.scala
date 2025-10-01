2a745b719fd984d4efeed4cf0d907856c957a869
lmugnai <11977337+lmugnai@users.noreply.github.com>
2025-10-01 15:39:11 +0100

    Update deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val hmrcBootstrapPlayVersion = "9.18.0"
-  private lazy val hmrcMongoPlayVersion     = "2.7.0"
+  private lazy val hmrcBootstrapPlayVersion = "10.2.0"
+  private lazy val hmrcMongoPlayVersion     = "2.9.0"
 
   private val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % hmrcBootstrapPlayVersion,
